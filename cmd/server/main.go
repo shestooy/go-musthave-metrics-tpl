2d18310f02d2000d3381f80a8e18bce5dc0294ca
Mikhail Kislov <kislov.mik@yandex.ru>
2024-07-30 14:51:31 +0300

    initial commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -26,7 +26,7 @@ func start() error {
 	r.Use(middleware.Recoverer)
 
 	r.Post("/update/{type}/{name}/{value}", handlers.PostMetrics)
-	r.Get("/value/{type}/{name}", handlers.GetMetricId)
+	r.Get("/value/{type}/{name}", handlers.GetMetricID)
 	r.Get("/", handlers.GetAllMetrics)
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
