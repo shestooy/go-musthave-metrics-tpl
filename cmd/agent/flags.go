0f85b685fb3ea75a9722a4c428dffa8a1ca375d6
shestooy <kmd017@campus.mephi.ru>
2024-08-10 01:08:34 +0300

    fix usage of flags

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -9,7 +9,7 @@ var (
 )
 
 func parseFlag() {
-	flag.StringVar(&agentEndPoint, "a", "http://localhost:8080", "address and port to run server")
+	flag.StringVar(&agentEndPoint, "a", "localhost:8080", "address and port to run agent")
 	flag.Int64Var(&reportInterval, "r", 10, "frequency of report metrics")
 	flag.Int64Var(&pollInterval, "p", 2, "the frequency of the metric survey")
 
