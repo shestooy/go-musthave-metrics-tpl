4e33bf3b592784a9c1fd49b88586f7a0b4659647
shestooy <kmd017@campus.mephi.ru>
2024-08-10 00:02:03 +0300

    default server flag value changed to "localhost:8080"

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -5,7 +5,7 @@ import "flag"
 var serverEndPoint string
 
 func parseFlag() {
-	flag.StringVar(&serverEndPoint, "a", ":8080", "address and port to run server")
+	flag.StringVar(&serverEndPoint, "a", "localhost:8080", "address and port to run server")
 
 	flag.Parse()
 }
