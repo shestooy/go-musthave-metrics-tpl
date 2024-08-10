c2ebea1fa44b43b6febddb07a4d6328cbfd8614b
shestooy <kmd017@campus.mephi.ru>
2024-08-10 16:44:26 +0300

    added the ability to work with env variables (iter 5)

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -1,6 +1,10 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+	"strconv"
+)
 
 var (
 	agentEndPoint  string
@@ -12,6 +16,15 @@ func parseFlag() {
 	flag.StringVar(&agentEndPoint, "a", "localhost:8080", "address and port to run agent")
 	flag.Int64Var(&reportInterval, "r", 10, "frequency of report metrics")
 	flag.Int64Var(&pollInterval, "p", 2, "the frequency of the metric survey")
-
 	flag.Parse()
+
+	if envAgentEndPoint := os.Getenv("ADDRESS"); envAgentEndPoint != "" {
+		agentEndPoint = envAgentEndPoint
+	}
+	if envReportInterval := os.Getenv("REPORT_INTERVAL"); envReportInterval != "" {
+		reportInterval, _ = strconv.ParseInt(envReportInterval, 10, 64)
+	}
+	if envPollInterval := os.Getenv("POLL_INTERVAL"); envPollInterval != "" {
+		pollInterval, _ = strconv.ParseInt(envPollInterval, 10, 64)
+	}
 }
--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -1,11 +1,17 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var serverEndPoint string
 
 func parseFlag() {
 	flag.StringVar(&serverEndPoint, "a", "localhost:8080", "address and port to run server")
-
 	flag.Parse()
+
+	if envServerEndPoint := os.Getenv("ADDRESS"); envServerEndPoint != "" {
+		serverEndPoint = envServerEndPoint
+	}
 }
