e57502b111262642ed8fad055c3a6a3b8b9c660d
Mikhail Kislov <kislov.mik@yandex.ru>
2024-08-21 20:49:44 +0300

    added new middleware logger

--- internal/httpserver/httpserver.go
+++ internal/httpserver/httpserver.go
@@ -1,10 +1,12 @@
 package httpserver
 
 import (
-	"fmt"
-	"github.com/shestooy/go-musthave-metrics-tpl.git/internal/flags"
+	f "github.com/shestooy/go-musthave-metrics-tpl.git/internal/flags"
 	"github.com/shestooy/go-musthave-metrics-tpl.git/internal/handlers"
+	l "github.com/shestooy/go-musthave-metrics-tpl.git/internal/logger"
+	"github.com/shestooy/go-musthave-metrics-tpl.git/internal/middlewares"
 	"github.com/shestooy/go-musthave-metrics-tpl.git/internal/storage"
+	"go.uber.org/zap"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -13,16 +15,19 @@ import (
 
 func Start() error {
 	storage.Storage.Init()
-	fmt.Printf("Server start on %s\n", flags.ServerEndPoint)
-	return http.ListenAndServe(flags.ServerEndPoint, GetRouter())
+	if err := l.Initialize(f.LogLevel); err != nil {
+		return err
+	}
+	l.Log.Info("Running server", zap.String("address", f.ServerEndPoint))
+
+	return http.ListenAndServe(f.ServerEndPoint, GetRouter())
 }
 
 func GetRouter() chi.Router {
 	r := chi.NewRouter()
 
-	r.Use(middleware.RequestID)
+	r.Use(middlewares.LoggingMiddleware)
 	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
 	r.Post("/update/{type}/{name}/{value}", handlers.PostMetrics)
---
+++ internal/logger/logger.go
@@ -0,0 +1,31 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var Log *zap.Logger = zap.NewNop()
+
+func Initialize(level string) error {
+	lvl, err := zap.ParseAtomicLevel(level)
+	if err != nil {
+		return err
+	}
+
+	cfg := zap.NewProductionConfig()
+
+	cfg.EncoderConfig.TimeKey = "time"
+
+	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	cfg.Level = lvl
+
+	zl, err := cfg.Build()
+	if err != nil {
+		return err
+	}
+
+	Log = zl
+	return nil
+}
