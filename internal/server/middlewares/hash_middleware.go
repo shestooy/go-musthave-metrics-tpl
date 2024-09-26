09d0b07d24981a2a02b6ace0df877e2855d5be95
shestooy <km.km.2021@inbox.ru>
2024-09-27 01:47:38 +0300

    improve logic of hash_middleware.go

--- internal/server/middlewares/hash_middleware.go
+++ internal/server/middlewares/hash_middleware.go
@@ -1,23 +1,18 @@
 package middlewares
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
+	"github.com/labstack/echo/v4"
 	"io"
 	"net/http"
-	"strings"
-
-	"github.com/labstack/echo/v4"
 )
 
 type responseWriter struct {
-	io.Writer
 	http.ResponseWriter
-}
-
-func (w *responseWriter) Write(b []byte) (int, error) {
-	return w.Writer.Write(b)
+	Body *bytes.Buffer
 }
 
 func hash(data []byte, key string) []byte {
@@ -32,36 +27,40 @@ func Hash(key string) echo.MiddlewareFunc {
 			if key == "" {
 				return next(c)
 			}
+
 			reqBodyHash := c.Request().Header.Get("HashSHA256")
 			if reqBodyHash == "" {
 				return next(c)
 			}
-			body, err := io.ReadAll(c.Request().Body)
-			if err != nil {
-				c.Error(err)
-			}
-			c.Request().Body = io.NopCloser(strings.NewReader(string(body)))
-			bodyHash := hash(body, key)
 
 			resHash, err := hex.DecodeString(reqBodyHash)
 			if err != nil {
-				c.Error(err)
+				return err
 			}
+
+			body, err := io.ReadAll(c.Request().Body)
+			if err != nil {
+				return err
+			}
+
+			c.Request().Body = io.NopCloser(io.MultiReader(bytes.NewReader(body)))
+			bodyHash := hash(body, key)
 
 			if !hmac.Equal(bodyHash, resHash) {
 				return c.JSON(http.StatusBadRequest, map[string]string{"err": "the hash checksum did not match"})
 			}
 
-			resBody := new(strings.Builder)
-			mw := io.MultiWriter(c.Response().Writer, resBody)
-			c.Response().Writer = &responseWriter{Writer: mw, ResponseWriter: c.Response().Writer}
-
+			res := &responseWriter{
+				ResponseWriter: c.Response().Writer,
+				Body:           &bytes.Buffer{},
+			}
+			c.Response().Writer = res
 			err = next(c)
 			if err != nil {
 				return err
 			}
 
-			responseBytes := []byte(resBody.String())
+			responseBytes := res.Body.Bytes()
 			resHash = hash(responseBytes, key)
 			c.Response().Header().Set("HashSHA256", hex.EncodeToString(resHash))
 
